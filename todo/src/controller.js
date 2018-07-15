2943037d972e7573b86bcff2b71e77c82a5efdd8
devJang <devjang2016@gmail.com>
2018-07-15 21:34:39 +0900

    Complete: TODO

--- todo/src/controller.js
+++ todo/src/controller.js
@@ -1,179 +1,183 @@
 import { fetchData, putData, updateData, deleteData } from './api.js';
 
 export default class Controller {
-	constructor(view, model) {
-		this.view = view;
-		this.model = model;
-		this.URL = 'https://mock-data-12c45.firebaseio.com';
-	}
+  constructor(view, model) {
+    this.view = view;
+    this.model = model;
+    this.URL = 'https://mock-data-12c45.firebaseio.com';
+  }
 
-	async init() {
-		const _ = this;
+  async init() {
+    const _ = this;
 
-		await _.loadTodoData();
-		const todo = await _.model.getTodoList();
-		const getTodoLength = status => _.model.getTodoLength(status);
-		const isActiveCount = getTodoLength(false);
-		const isCompletedCount = getTodoLength(true);
+    await _.loadTodoData();
+    const todo = await _.model.getTodoList();
+    const getTodoLength = status => _.model.getTodoLength(status);
+    const isActiveCount = getTodoLength(false);
+    const isCompletedCount = getTodoLength(true);
 
-		await _.view.init(todo);
-		_.toggleFooter();
-		_.view.updateTodoCount(isActiveCount);
-		_.toggleClsCompleted();
-	}
+    await _.view.init(todo);
+    _.toggleFooter();
+    _.view.updateTodoCount(isActiveCount);
+    _.toggleClsCompleted();
+  }
 
-	async loadTodoData() {
-		const result = await fetchData(`${this.URL}/todo.json`);
+  async loadTodoData() {
+    const result = await fetchData(`${this.URL}/todo.json`);
 
-		this.mappingTodoData(result);
-	}
+    this.mappingTodoData(result);
+  }
 
-	mappingTodoData(todo) {
-		this.model.setTodoList(todo);
-	}
+  mappingTodoData(todo) {
+    this.model.setTodoList(todo);
+  }
 
-	async addTodo({ target: $NEW_TEXT_INPUT, keyCode }) {
-		if (keyCode !== 13 || !$NEW_TEXT_INPUT.value) return;
-		console.log('adasdas');
+  async addTodo({ target: $NEW_TEXT_INPUT, keyCode }) {
+    if (keyCode !== 13 || !$NEW_TEXT_INPUT.value) return;
 
-		const _ = this;
-		const todoList = _.model.getTodoList();
-		const newTodo = {
-			content: $NEW_TEXT_INPUT.value,
-			completed: false,
-		};
-		const newTodoId = await putData(`${this.URL}/todo.json`, newTodo);
-		newTodo.id = newTodoId;
+    const _ = this;
+    const todoList = _.model.getTodoList();
+    const newTodo = {
+      content: $NEW_TEXT_INPUT.value,
+      completed: false,
+    };
+    const newTodoId = await putData(`${this.URL}/todo.json`, newTodo);
+    newTodo.id = newTodoId;
 
-		_.model.addTodo(newTodo);
-		_.view.addTodo(newTodo);
-		_.toggleFooter();
+    _.model.addTodo(newTodo);
+    _.view.addTodo(newTodo);
+    _.toggleFooter();
 
-		const activeCount = _.model.getTodoLength(false);
-		_.view.updateTodoCount(activeCount);
+    const activeCount = _.model.getTodoLength(false);
+    _.view.updateTodoCount(activeCount);
+    $NEW_TEXT_INPUT.value = '';
+  }
 
-		$NEW_TEXT_INPUT.value = '';
-	}
+  removeTodo($LI) {
+    const _ = this;
+    const selectedId = $LI.id;
+    const todoList = _.model.getTodoList();
 
-	removeTodo($LI) {
-		const _ = this;
-		const selectedId = $LI.id;
-		const todoList = _.model.getTodoList();
+    for (const id in todoList) {
+      if (selectedId === id) {
+        deleteData(`${this.URL}/todo/${id}.json`);
+      }
+    }
 
-		for (const id in todoList) {
-			if (selectedId === id) {
-				deleteData(`${this.URL}/todo/${id}.json`);
-			}
-		}
+    _.model.removeTodo(selectedId);
+    _.view.removeTodo($LI);
+    _.toggleFooter();
 
-		// _.model.removeTodo(selectedId);
-		_.view.removeTodo($LI);
-		_.toggleFooter();
+    const activeCount = _.model.getTodoLength(false);
+    _.view.updateTodoCount(activeCount);
+    _.toggleClsCompleted();
+  }
 
-		const activeCount = _.model.getTodoLength(false);
-		_.view.updateTodoCount(activeCount);
-		_.toggleClsCompleted();
-	}
+  showEditMode(e) {
+    this.view.showEditMode(e);
+  }
 
-	showEditMode(e) {
-		this.view.showEditMode(e);
-	}
+  updateTodo({ target: $EDIT_TEXT_INPUT, keyCode }) {
+    if (keyCode !== 13 || !$EDIT_TEXT_INPUT.value) return;
+    const _ = this;
+    const $LI = $EDIT_TEXT_INPUT.parentNode;
+    const todoList = _.model.getTodoList();
+    const $label = $LI.querySelector('label');
 
-	updateTodo({ target: $EDIT_TEXT_INPUT, keyCode }) {
-		if (keyCode !== 13 || !$EDIT_TEXT_INPUT.value) return;
-		const _ = this;
-		const $LI = $EDIT_TEXT_INPUT.parentNode;
-		const todoList = _.model.getTodoList();
-		const $label = $LI.querySelector('label');
+    for (const id in todoList) {
+      if (id === $LI.id) {
+        updateData(`${this.URL}/todo.json`, {
+          [id]: {
+            ...todoList[id],
+            content: $EDIT_TEXT_INPUT.value,
+          },
+        });
+        _.model.updateTodo(id, $EDIT_TEXT_INPUT.value);
+        $label.textContent = $EDIT_TEXT_INPUT.value;
+        _.view.hideEditMode($EDIT_TEXT_INPUT, $label);
+      }
+    }
+  }
 
-		for (const id in todoList) {
-			if (id === $LI.id) {
-				updateData(`${this.URL}/todo.json`, {
-					[id]: {
-						...todoList[id],
-						content: $EDIT_TEXT_INPUT.value,
-					},
-				});
-				_.model.updateTodo(id, $EDIT_TEXT_INPUT.value);
-				$label.textContent = $EDIT_TEXT_INPUT.value;
-				_.view.hideEditMode($EDIT_TEXT_INPUT, $label);
-			}
-		}
-	}
+  toggleState(target, status = undefined) {
+    const _ = this;
+    const $LI = status === undefined ? target.parentNode : target;
+    const selectedId = $LI.id;
+    const todoList = _.model.getTodoList();
+    const { completed: originCompleted } = _.model.getTodoList(selectedId);
 
-	hideEditMode($EDIT_TEXT_INPUT) {
-		this.view.hideEditMode(
-			$EDIT_TEXT_INPUT,
-			$EDIT_TEXT_INPUT.parentNode.querySelector('label'),
-		);
-	}
+    updateData(`${this.URL}/todo.json`, {
+      [selectedId]: {
+        ...todoList[selectedId],
+        completed: !originCompleted,
+      },
+    });
+    _.model.toggleState(selectedId, status);
+    _.toggleClsCompleted();
 
-	toggleState(target, status = undefined) {
-		const _ = this;
-		const $LI = status === undefined ? target.parentNode : target;
-		const id = Number($LI.id);
-		const todoList = _.model.getTodoList();
-		const targetIndex = todoList.findIndex(todo => todo.id === id);
+    const activeCount = _.model.getTodoLength(false);
 
-		_.model.toggleState(targetIndex, status);
-		_.toggleClsCompleted();
+    _.view.toggleState($LI, selectedId);
+    _.view.updateTodoCount(activeCount);
+    status === undefined && _.view.toggleToggleAllBtn(false);
+  }
 
-		const activeCount = _.model.getTodoLength(false);
+  clearCompleted($COMPLETED_TODO) {
+    [...$COMPLETED_TODO].map($TODO => this.removeTodo($TODO));
+  }
 
-		_.view.toggleState($LI, id);
-		_.view.updateTodoCount(activeCount);
-		status === undefined && _.view.toggleToggleAllBtn(false);
-	}
+  hideEditMode($EDIT_TEXT_INPUT) {
+    this.view.hideEditMode(
+      $EDIT_TEXT_INPUT,
+      $EDIT_TEXT_INPUT.parentNode.querySelector('label'),
+    );
+  }
 
-	clearCompleted($COMPLETED_TODO) {
-		[...$COMPLETED_TODO].map($TODO => this.removeTodo($TODO));
-	}
+  toggleFooter() {
+    const _ = this;
+    const isTodoList = !!_.model.getTodoLength() ? 'block' : 'none';
 
-	toggleFooter() {
-		const _ = this;
-		const isTodoList = !!_.model.getTodoLength() ? 'block' : 'none';
+    _.view.toggleFooter(isTodoList);
+  }
 
-		_.view.toggleFooter(isTodoList);
-	}
+  toggleClsCompleted() {
+    const _ = this;
+    const getTodoLength = status => _.model.getTodoLength(status);
+    const isCompletedCount = getTodoLength(true);
 
-	toggleClsCompleted() {
-		const _ = this;
-		const getTodoLength = status => _.model.getTodoLength(status);
-		const isCompletedCount = getTodoLength(true);
+    _.view.toggleClsCompleted(isCompletedCount >= 1 ? 'block' : 'none');
+  }
 
-		_.view.toggleClsCompleted(isCompletedCount >= 1 ? 'block' : 'none');
-	}
+  changeFilter($FILTERS, $TODO_LIST, $SELECTED_BTN) {
+    const [selectedState] = $SELECTED_BTN.href.split('/').slice(-1);
 
-	changeFilter($FILTERS, $TODO_LIST, $SELECTED_BTN) {
-		const [selectedState] = $SELECTED_BTN.href.split('/').slice(-1);
+    [...$FILTERS].map($A => {
+      $A.classList.value === 'selected' && this.view.removeSelectedFilter($A);
+    });
 
-		[...$FILTERS].map($A => {
-			$A.classList.value === 'selected' && this.view.removeSelectedFilter($A);
-		});
+    [...$TODO_LIST].map($LI => {
+      let result = 'list-item';
+      if (selectedState === 'active') {
+        result = $LI.classList.value === 'completed' ? 'none' : 'list-item';
+      }
+      if (selectedState === 'completed') {
+        result = $LI.classList.value === 'completed' ? 'list-item' : 'none';
+      }
+      this.view.toogleDisplay($LI, result);
+    });
+    this.view.addSelectedFilter($SELECTED_BTN);
+  }
 
-		[...$TODO_LIST].map($LI => {
-			let result = 'list-item';
-			if (selectedState === 'active') {
-				result = $LI.classList.value === 'completed' ? 'none' : 'list-item';
-			}
-			if (selectedState === 'completed') {
-				result = $LI.classList.value === 'completed' ? 'list-item' : 'none';
-			}
-			this.view.toogleDisplay($LI, result);
-		});
-		this.view.addSelectedFilter($SELECTED_BTN);
-	}
+  toggleAll($TOGGLE_ALL, $TODO_LIST) {
+    const isChecked = $TOGGLE_ALL.checked;
+    const isCompletedList = $LI =>
+      isChecked
+        ? $LI.classList.value !== 'completed'
+        : $LI.classList.value === 'completed';
 
-	toggleAll($TOGGLE_ALL, $TODO_LIST) {
-		const isChecked = $TOGGLE_ALL.checked;
-		const isCompletedList = $LI =>
-			isChecked
-				? $LI.classList.value !== 'completed'
-				: $LI.classList.value === 'completed';
-
-		[...$TODO_LIST].filter(isCompletedList).map($LI => {
-			this.toggleState($LI, isChecked);
-			this.view.toggleChecked($LI.querySelector('.toggle'), isChecked);
-		});
-	}
+    [...$TODO_LIST].filter(isCompletedList).map($LI => {
+      this.toggleState($LI, isChecked);
+      this.view.toggleChecked($LI.querySelector('.toggle'), isChecked);
+    });
+  }
 }
--- todo/src/model.js
+++ todo/src/model.js
@@ -1,40 +1,40 @@
 export default class Model {
-	getTodoList(id) {
-		return id ? this.todoList[id] : this.todoList;
-	}
+  getTodoList(id) {
+    return id ? this.todoList[id] : this.todoList;
+  }
 
-	getTodoLength(status = 'All') {
-		const { todoList } = this;
-		let isLength = 0;
+  getTodoLength(status = 'All') {
+    const { todoList } = this;
+    let isLength = 0;
 
-		for (const i in todoList) {
-			todoList[i].completed === status && isLength++;
-		}
-		return status === 'All' ? Object.keys(todoList).length : isLength;
-	}
+    for (const i in todoList) {
+      todoList[i].completed === status && isLength++;
+    }
+    return status === 'All' ? Object.keys(todoList).length : isLength;
+  }
 
-	setTodoList(todoList) {
-		this.todoList = todoList;
-	}
+  setTodoList(todoList) {
+    this.todoList = todoList;
+  }
 
-	addTodo({ id, content, completed }) {
-		this.todoList[id] = { content, completed };
-	}
+  addTodo({ id, content, completed }) {
+    this.todoList[id] = { content, completed };
+  }
 
-	removeTodo(id) {
-		if (this.todoList.length === 1) {
-			this.todoList.length = 0;
-		} else {
-			this.todoList.splice(id, 1);
-		}
-	}
+  removeTodo(id) {
+    if (this.todoList.length === 1) {
+      this.todoList = {};
+    } else {
+      delete this.todoList[id];
+    }
+  }
 
-	updateTodo(id, newContent) {
-		this.todoList[id].content = newContent;
-	}
+  updateTodo(id, newContent) {
+    this.todoList[id].content = newContent;
+  }
 
-	toggleState(id, status = undefined) {
-		this.todoList[id].completed =
-			status === undefined ? !this.todoList[id].completed : status;
-	}
+  toggleState(id, status = undefined) {
+    this.todoList[id].completed =
+      status === undefined ? !this.todoList[id].completed : status;
+  }
 }
